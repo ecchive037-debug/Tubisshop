8c301ac58468007681a8031e4200a2832e0fc17c
Chandan Chaurasiya <your_email@example.com>
2025-12-17 16:40:24 +0530

    backend checked

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ const notificationRoute = require('../src/Routes/notification.route')
 
 
 app.use(cors({
-    origin: ["https://vercel.com/buimb-digitals-projects/tubisshop"],
+    origin: ["http://localhost:5173"],
     credentials: true
 }));
 
